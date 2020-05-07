7752c5a7d29b81f52b3ad63488937e2921333eeb
Jatin Goyal <2015csb1014@iitrpr.ac.in>
2020-05-07 15:27:29 +0530

    Adds User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 # Register your models here.
+
+
+class MyUserAdmin(BaseUserAdmin):
+    list_display = ('uid', 'username', 'name',
+                    'email', 'admin',)
+    list_filter = ('admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('name', 'email',)}),
+        ('Permissions', {'fields': ('admin', 'staff', 'active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'username', 'email', 'password1', 'password2')}),
+    )
+    ordering = ('uid',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, MyUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,95 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.core.validators import RegexValidator
 # Create your models here.
+
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, name, email, password=None):
+        if not name:
+            raise ValueError("User must have a name")
+        if not email:
+            raise ValueError("User must have an email id")
+        if not username:
+            raise ValueError("User must have a username")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user = self.model(
+            username=username,
+            name=name,
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, username, name, email, password=None):
+        user = self.create_user(
+            username=username,
+            name=name,
+            email=email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, name, email, password=None):
+        user = self.create_user(
+            username=username,
+            name=name,
+            email=email,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    uid = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    username = models.CharField(max_length=255, unique=True, validators=[alphanumeric], db_index=True)
+    name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'email']
+
+    objects = UserManager()
+
+    class Meta:
+        app_label = 'users'
+
+    def get_name(self):
+        return self.name
+
+    def get_email(self):
+        return self.email
+
+    def get_username(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
